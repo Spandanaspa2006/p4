8cd3c4eb451f8b0d675d77ae17209dd8bf3a7dd1
Spandanaspa2006 <165010762+Spandanaspa2006@users.noreply.github.com>
2024-03-27 15:53:06 +0530

    Update ms.py

--- ms.py
+++ ms.py
@@ -2,7 +2,7 @@ class Node:
     def __init__ (self,data):
         self.data=data
         self.next=None
-        self.prev=None
+        self.prev=Noneaaaaaaaaaaaaaaaaa
 class doubly:
     def __init__ (self):
         self.head=None
